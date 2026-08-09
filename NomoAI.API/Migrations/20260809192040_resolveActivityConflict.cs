945c3571fc1a2ff689ab38c0ac39f2f9d033984c
khaledmo07 <kmoamed942@gmail.com>
2026-08-09 22:29:20 +0300

    Resolve Activity Conflict

---
+++ NomoAI.API/Migrations/20260809192040_resolveActivityConflict.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NomoAI.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class resolveActivityConflict : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "EXEC sp_rename 'Activities.canMakeSession', 'CanMakeSession_tmp', 'COLUMN';");
+            migrationBuilder.Sql(
+                "EXEC sp_rename 'Activities.CanMakeSession_tmp', 'CanMakeSession', 'COLUMN';");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "EXEC sp_rename 'Activities.CanMakeSession', 'canMakeSession_tmp', 'COLUMN';");
+            migrationBuilder.Sql(
+                "EXEC sp_rename 'Activities.canMakeSession_tmp', 'canMakeSession', 'COLUMN';");
+        }
+    }
+}
